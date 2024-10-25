2f9d0067d30c4898ca7c4555faa88b4c21127e9c
xiaobei <5214444887@163.com>
2024-10-25 07:34:38 -0700

    testModel

---
+++ HCDeviceModel/Sources/HCDeviceModel/HCDeviceModel.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct HCDeviceModel {
+    public var text = "hello"
+    
+    public init() {
+        
+    }
+}
---
+++ HCDeviceModel/Tests/HCDeviceModelTests/HCDeviceModelTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import HCDeviceModel
+
+final class HCDeviceModelTests: XCTestCase {
+    func testExample() throws {
+        let model = HCDeviceModel()
+        print(model.text)
+    }
+}
