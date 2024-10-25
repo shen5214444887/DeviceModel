da86ae204f8fea4f41555efcfc9b1e36d2e51e8b
xiaobei <5214444887@163.com>
2024-10-25 07:45:36 -0700

    add path

--- HCDeviceModel/Package.swift
+++ HCDeviceModel/Package.swift
@@ -16,9 +16,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HCDeviceModel"),
+            name: "HCDeviceModel",
+            path: "Source"),
         .testTarget(
             name: "HCDeviceModelTests",
-            dependencies: ["HCDeviceModel"]),
+            dependencies: ["HCDeviceModel"],
+            path: "Tests"),
     ]
 )
